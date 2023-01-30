69b5f417d802c294e0d1ff64d80bd659e237814f
YaredFall <yaredfall@gmail.com>
2023-01-31 02:23:41 +0700

    ~anime page query initialData fix

--- react/src/Api/useAnimeJoyAnimePageQuery.ts
+++ react/src/Api/useAnimeJoyAnimePageQuery.ts
@@ -1,19 +1,24 @@
 import { useQuery } from "react-query";
 import mockupPage from "../devMockup/mockupAnimePage.html?raw";
+import { useRef } from "react";
 
 const parser = new DOMParser();
 
 export const useAnimeJoyAnimePageQuery = (animejoyFullID: string) => {
 
+    const isNotFetchedYet = useRef(true);
+
     return useQuery(
         ['animejoy', 'anime', animejoyFullID],
         () => {
             if (import.meta.env.DEV)
-                return parser.parseFromString(mockupPage, "text/html");;
+                return parser.parseFromString(mockupPage, "text/html");
+
+            isNotFetchedYet.current = false;
 
             return fetch(`https://animejoy.ru/tv-serialy/${animejoyFullID}`)
                 .then(response => response.text().then(page => parser.parseFromString(page, "text/html")))
         },
-        { retry: false, initialData: document  }
+        { retry: false, initialData: isNotFetchedYet.current ? document : undefined}
     )
 }
