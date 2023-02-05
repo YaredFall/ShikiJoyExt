57f35c3a9f61703cc3ef47fe68a189bd79705f2c
YaredFall <yaredfall@gmail.com>
2023-02-05 23:37:16 +0700

    ~SideNav Link -> NavLink; +SideNav icon link labels; +new SideNav links (temporarily non-fucntional); ~adjustments

--- react/src/Components/SideNav.tsx
+++ react/src/Components/SideNav.tsx
@@ -1,26 +1,62 @@
 import { FC } from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { appRoutes } from "../Utils/appRoutes";
-import radishIcon from "/images/radish256x256.png"
-import { IoSettingsOutline } from "react-icons/all";
-import styles from "./SideNav.module.scss"
+import radishIcon from "/images/radish256x256.png";
+import { BsArrowCounterclockwise, IoIosLogIn, IoSearchOutline, IoSettingsOutline, TbList } from "react-icons/all";
+import styles from "./SideNav.module.scss";
 
 //@ts-ignore
-const radishIconExt = chrome.runtime?.getURL("react/dist/images/radish256x256.png")
+const radishIconExt = chrome.runtime?.getURL("react/dist/images/radish256x256.png");
 
-type SideNavProps = {
-    
-}
+type SideNavProps = {}
 
-const SideNav:FC<SideNavProps> = () => {
+const SideNav: FC<SideNavProps> = () => {
+
     return (
         <header className={styles.headerContainer}>
             <nav className={styles.navbar}>
-                <Link className={styles.navlink} to={appRoutes.home} children={<img src={radishIconExt || radishIcon} title={"Главная"} alt={"Главная"} />} />
-                <Link className={styles.navlink} to={appRoutes.test} children={<div>Test</div>} />
-                <Link className={styles.navlink} to={appRoutes.test2} children={<div>Test 2</div>} />
-                <Link className={styles.navlink} to={appRoutes.nonexistent} children={<div>Nonexistent</div>} />
-                <Link className={`${styles.navlink} ${styles.bottom}`} to={""} children={<IoSettingsOutline />} />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={appRoutes.home}
+                         data-label={"Главная"}
+                         children={<img className={styles.radish} src={radishIconExt || radishIcon} alt={"Лого"} />}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={"tbd"}
+                         data-label={"Поиск"}
+                         children={<IoSearchOutline />}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={"tbd"}
+                         data-label={"Мой список"}
+                         children={<TbList />}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${styles.smaller} ${isActive ? styles.active : ""}`}
+                         to={"tbd"}
+                         data-label={"Последнее"}
+                         children={<BsArrowCounterclockwise />}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={appRoutes.test}
+                         children={<div>Test</div>}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={appRoutes.test2}
+                         children={<div>Test 2</div>}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={appRoutes.nonexistent}
+                         children={<div>404</div>}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${styles.bottom} ${isActive ? styles.active : ""}`}
+                         to={"tbd"}
+                         data-label={"Вход"}
+                         children={<IoIosLogIn />}
+                />
+                <NavLink className={({ isActive }) => `${styles.navlink} ${isActive ? styles.active : ""}`}
+                         to={"tbd"}
+                         data-label={"Настройки"}
+                         children={<IoSettingsOutline />}
+                />
             </nav>
         </header>
     );
