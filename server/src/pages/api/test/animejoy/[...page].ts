61dfc23084eb9b35b9bae1fd666e9e905b90ced3
YaredFall <yaredfall@gmail.com>
2023-03-18 01:28:58 +0700

    ~fixed animejoy api request body handling

--- server/src/pages/api/test/animejoy/[...page].ts
+++ server/src/pages/api/test/animejoy/[...page].ts
@@ -1,19 +1,38 @@
 import got from 'got'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { isArray } from "util";
+import formidable from "formidable";
 
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const { page, ...query } = req.query;
-    const url = (Array.isArray(page) ? page?.join('/') : page);
-    try {
-        const response = await got(`https://animejoy.ru/${url + "/"}${
-            Object.entries(query).length ? "?" + Object.entries(query).map(e => e[0] + "=" + e[1]).join('&') : ""}`).text()
-        res.status(200).send(response)
-    } catch (err) {
-        res.status(500).send(`<h1>${err}</h1>`)
-    }
+    return new Promise((resolve, reject) => {
+        const { page, ...query } = req.query;
+
+        const url = (Array.isArray(page) ? page?.join('/') : page);
+        try {
+            const form = new formidable.IncomingForm();
+            form.parse(req, async function (err, fields, files) {
+                const response = await got.post(`https://animejoy.ru/${url + "/"}${
+                        Object.entries(query).length ? "?" + Object.entries(query).map(e => e[0] + "=" + e[1]).join('&') : ""}`,
+                    {
+                        form: fields
+                    }
+                ).text()
+                res.status(200).send(response);
+                resolve(0);
+            });
+        } catch (err) {
+            res.status(500).send(`<h1>${err}</h1>`);
+            console.error(err);
+            resolve(0)
+        }
+    })
 }
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
