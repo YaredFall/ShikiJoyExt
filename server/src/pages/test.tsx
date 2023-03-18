d73238806d7d49741af70e52ca3b7556850960c9
YaredFall <yaredfall@gmail.com>
2023-03-18 18:59:09 +0700

    ~a bit of testing; ~changed region back to arn

--- server/src/pages/test.tsx
+++ server/src/pages/test.tsx
@@ -1,18 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          Test
-        </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <main className={styles.main}>
+                <div className={styles.description}>
+                    <Link href={"/"}>Back to home page</Link>
+                </div>
+                <div className={styles.center}>
+                    <p>Test page</p><br/>
+                </div>
+                <div></div>
+            </main>
+        </>
+    );
 }
