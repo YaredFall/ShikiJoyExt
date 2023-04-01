48e410d98e714be637e818799da159f8d5c4521b
YaredFall <yaredfall@gmail.com>
2023-04-01 23:55:32 +0700

    ~LoadingPage now can be either positon:fixed or :absolute

--- react/src/Pages/LoadingPage.tsx
+++ react/src/Pages/LoadingPage.tsx
@@ -6,10 +6,10 @@ import radishIcon from "/images/radish256x256.png";
 const radishIconExt = chrome.runtime?.getURL("bundled/images/radish256x256.png");
 
 type LoadingPageProps = {
-    
+    fullscreen?: boolean
 }
 
-const LoadingPage:FC<LoadingPageProps> = () => {
+const LoadingPage:FC<LoadingPageProps> = ({ fullscreen = true}) => {
     return (
         <div className={`loader ${styles.container}`}>
             <img className={styles.logo} src={radishIconExt || radishIcon} alt={"Лого"} />
