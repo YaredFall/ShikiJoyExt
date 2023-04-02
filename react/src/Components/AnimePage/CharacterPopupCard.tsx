305563c85b5324a384c08991082a82f95dfdf6f3
YaredFall <yaredfall@gmail.com>
2023-04-02 15:47:54 +0700

    +char popup content and layout

--- react/src/Components/AnimePage/CharacterPopupCard.tsx
+++ react/src/Components/AnimePage/CharacterPopupCard.tsx
@@ -21,7 +21,7 @@ const CharacterPopupCard: FC<CharacterLargeCardProps> = ({ isOpen, id, bindNode,
             return ky((import.meta.env.DEV ? ApiLinks.get("dev/shikijoy") : ApiLinks.get("shikijoy")) + `api/shikimori/characters/${id}`)
                 .json();
         },
-        { 
+        {
             ...defautlQueryConfig,
             enabled: isOpen
         }
@@ -45,7 +45,7 @@ const CharacterPopupCard: FC<CharacterLargeCardProps> = ({ isOpen, id, bindNode,
             window.removeEventListener("resize", handler, true);
         };
     }, []);
-    
+
     let timeout: number | undefined = undefined;
 
     return (
@@ -53,19 +53,47 @@ const CharacterPopupCard: FC<CharacterLargeCardProps> = ({ isOpen, id, bindNode,
             <div className={styles.container}
                  style={{ top: position.top + "px", left: position.left + "px" }}
                  onMouseOver={() => {
-                     clearTimeout(timeout)
+                     clearTimeout(timeout);
                      setIsInside && setIsInside(true);
                  }}
                  onMouseLeave={() => {
                      timeout = setTimeout(() => {
                          setIsInside && setIsInside(false);
-                     }, 500)
+                     }, 500);
                  }}
             >
-                {data ? JSON.stringify(data) : <LoadingPage fullscreen={false} />}
+                {data ? <Card data={data} /> : <LoadingPage fullscreen={false} />}
             </div>
         </PopupPortal>
     );
 };
 
 export default CharacterPopupCard;
+
+function Card({ data }: { data: any }) {
+    return (
+        <>
+            {/*<img src={"https://shikimori.one" + data.image.original} alt={""} />*/}
+            <div className={styles.charDesc}>
+                <h4 className={styles.name} children={data.russian || data.name} />
+                <p children={data.description || "Описание отсутствует"} />
+            </div>
+            <div className={styles.animeAndSeyu}>
+                <div>
+                    <h5>Аниме</h5>
+                    <a href={"https://shikimori.one" + data.animes[0].url} title={data.animes[0].russian}>
+                        <img src={"https://shikimori.one" + data.animes[0].image.x96} alt={""} />
+                    </a>
+                </div>
+                {data.seyu[0] &&
+                    <div>
+                        <h5>Сейю</h5>
+                        <a href={"https://shikimori.one" + data.seyu[0].url} title={data.seyu[0].russian}>
+                            <img src={"https://shikimori.one" + data.seyu[0].image.x96} alt={""} />
+                        </a>
+                    </div>
+                }
+            </div>
+        </>
+    );
+}
