4a2b64d96d8a7431124b5d0d2f1353fd1afcd93b
YaredFall <yaredfall@gmail.com>
2023-04-08 23:49:17 +0700

    +preventing popup from going out of window; ~adjustments

--- react/src/Components/PopupWithTrigger.tsx
+++ react/src/Components/PopupWithTrigger.tsx
@@ -1,5 +1,9 @@
-import { FC, ReactNode, RefObject, useEffect, useState } from 'react';
+import { CSSProperties, FC, ReactNode, RefObject, useEffect, useRef, useState } from 'react';
 import PopupPortal from "./PopupPortal";
+
+const POPUP_WIDTH = 480;
+const POPUP_HEIGHT = 290;
+const POPUP_MARGIN = 20;
 
 type PopupWithTriggerProps = {
     triggerRef: RefObject<HTMLElement>
@@ -50,41 +54,63 @@ const PopupWithTrigger: FC<PopupWithTriggerProps> = ({ triggerRef, children, con
         };
     }, [isMouseInsideTrigger]);
 
-    const [position, setPosition] = useState({
-        top: ~~((triggerRef?.current?.getBoundingClientRect().top || 0) + document.documentElement.scrollTop),
-        left: ~~(triggerRef?.current?.getBoundingClientRect().right || 0)
+    const [popupCSSProps, setPopupCSSProps] = useState({
+        "--top": "0px",
+        "--left": "0px",
+        "--width": `${POPUP_WIDTH}px`,
+        "--height": `${POPUP_HEIGHT}px`,
+        "--margin": `${POPUP_MARGIN}px`
     });
 
     useEffect(() => {
         const handler = () => {
-            triggerRef && setPosition(prev => ({
-                top: ~~((triggerRef?.current?.getBoundingClientRect().top || 0) + document.documentElement.scrollTop),
-                left: ~~(triggerRef?.current?.getBoundingClientRect().right || 0)
-            }));
+            const triggerRect = triggerRef?.current?.getBoundingClientRect();
+            if (triggerRect) {
+                const topLimit = document.documentElement.scrollTop + window.innerHeight - POPUP_HEIGHT - 2;
+
+                //right
+                let top: number = Math.min(~~((triggerRect.top || POPUP_MARGIN) + document.documentElement.scrollTop), topLimit);
+                let left: number = ~~(triggerRect.right || 84) + POPUP_MARGIN; // 84 is SideNav width
+
+                if (left + POPUP_WIDTH + POPUP_MARGIN > window.innerWidth) {
+                    //left
+                    left = ~~(triggerRect.left || 0) - POPUP_MARGIN - POPUP_WIDTH;
+
+                    if (left < 84 + 2) { // 84 is SideNav width
+                        //down
+                        left = ~~(triggerRect.left || 0);
+                        left -= Math.max(0, left + POPUP_WIDTH + POPUP_MARGIN - window.innerWidth);
+                        top = Math.min(top + triggerRect.height + POPUP_MARGIN, topLimit);
+                    }
+                }
+
+                setPopupCSSProps(prev => ({
+                    ...prev,
+                    "--top": `${top}px`,
+                    "--left": `${left}px`
+                }));
+            }
         };
         handler();
         window.addEventListener("resize", handler, true);
         return () => {
             window.removeEventListener("resize", handler, true);
         };
-    }, []);
+    }, [isHidden, isPopupOpen]);
 
     let timeout: number | undefined = undefined;
 
+    const containerRef = useRef<HTMLDivElement>(null);
 
     return (
         <PopupPortal isOpen={isPopupOpen || isMouseInsidePopup}>
             <div className={`${containerClassName} ${isHidden ? "remove" : ""}`}
-                 style={{ top: position.top + "px", left: position.left + "px" }}
+                 ref={containerRef}
+                 style={popupCSSProps as CSSProperties}
                  onMouseOver={(e) => {
-                     const hoveredThis = (e.target as Element).closest(`div[style="top: ${position.top}px; left: ${position.left}px;"]`);
-                     if (hoveredThis) {
-                         setTimeout(() => {
-                             clearTimeout(timeout);
-                             timeout = undefined;
-                             setIsMouseInsidePopup(true);
-                         }, 0);
-                     }
+                     clearTimeout(timeout);
+                     timeout = undefined;
+                     setIsMouseInsidePopup(true);
                  }}
                  onMouseLeave={() => {
                      timeout = setTimeout(() => {
@@ -93,8 +119,8 @@ const PopupWithTrigger: FC<PopupWithTriggerProps> = ({ triggerRef, children, con
                      }, 500);
                  }}
                  onMouseMove={(e) => {
-                     const hoveredThis = (e.target as Element).closest(`div[style="top: ${position.top}px; left: ${position.left}px;"]`);
-                     if (!hoveredThis && !isHidden && !timeout) {
+                     const hoveringThis = containerRef.current?.contains(e.target as Node);
+                     if (!hoveringThis && !isHidden && !timeout) {
                          timeout = setTimeout(() => {
                              setIsHidden(true);
                              timeout = undefined;
