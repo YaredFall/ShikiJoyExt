56261f9ece2e02282bcec1e0978049519b58602d
YaredFall <yaredfall@gmail.com>
2023-04-11 17:54:26 +0700

    +fixed default scroll on Space keydown; ~removed unused code

--- react/src/Components/AnimePage/Player/hooks/usePlayersFixes.ts
+++ react/src/Components/AnimePage/Player/hooks/usePlayersFixes.ts
@@ -1,15 +1,8 @@
 import { RefObject, useEffect, useRef } from "react";
 
 export const usePlayersFixes = (iframeRef: RefObject<HTMLIFrameElement>) => {
-
-    const mouseJustEntered = useRef(true);
-
+    
     useEffect(() => {
-        //autofocus iframe on page load
-        // const onLoad = () => {
-        //     iframeRef.current?.focus()
-        // };
-
         const onKeyUp = (e: KeyboardEvent) => {
             if (!e.metaKey && !e.altKey && !e.shiftKey && !e.ctrlKey) {
                 if (document.activeElement === document.body && (e.code === "KeyF" || e.code === "Space")) {
@@ -18,38 +11,18 @@ export const usePlayersFixes = (iframeRef: RefObject<HTMLIFrameElement>) => {
                 }
             }
         };
-
-        //autofocus iframe when window gets focused
-        // const onFocus = () => {
-        //     if (mouseJustEntered.current) {
-        //         // console.log("active", document.activeElement)
-        //         mouseJustEntered.current = false;
-        //         setTimeout(() => {
-        //             // console.log("in timeout active", document.activeElement)
-        //             if (document.activeElement === document.body) {
-        //                 iframeRef.current?.focus();
-        //                 // console.log("focus", document.activeElement)
-        //             }
-        //         }, 0);
-        //     }
-        // };
-
-        //helps to detect when window gets focus
-        // const onMouseEnter = () => {
-        //     if (!document.hasFocus() && !mouseJustEntered.current) {
-        //         mouseJustEntered.current = true;
-        //     }
-        // };
-
-        //window.addEventListener("load", onLoad);
+        
+        const onKeyDown = (e: KeyboardEvent) => {
+            if ((document.activeElement === iframeRef.current || document.activeElement === document.body) && e.code === "Space") {
+                e.preventDefault();
+            }
+        }
+        
         document.addEventListener("keyup", onKeyUp);
-        //document.body.addEventListener("mouseenter", onMouseEnter);
-        //window.addEventListener("focus", onFocus);
+        document.addEventListener("keydown", onKeyDown);
         return () => {
-            //window.removeEventListener("load", onLoad);
             document.removeEventListener("keyup", onKeyUp);
-            //window.removeEventListener("focus", onFocus);
-            //document.body.removeEventListener("mouseenter", onMouseEnter);
+            document.removeEventListener("keydown", onKeyDown);
         };
     }, []);
 };
