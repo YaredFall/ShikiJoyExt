f194f1c65561c80a1ae15d977c48b43b64a2f37a
YaredFall <yaredfall@gmail.com>
2023-05-11 20:28:25 +0700

    ~Picture image load retry

--- react/src/Components/Common/Picture.tsx
+++ react/src/Components/Common/Picture.tsx
@@ -12,6 +12,8 @@ import {
 } from 'react';
 import styles from './Picture.module.scss';
 
+const MAX_RETRIES = 3;
+
 type PictureProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
 
 const Picture: FC<PictureProps> = ({
@@ -39,18 +41,46 @@ const Picture: FC<PictureProps> = ({
         if (src) {
             loadedImages.current.add(src);
         }
-        onLoad && onLoad(e)
+        onLoad && onLoad(e);
     }, [src]);
 
     const onTransitionEnd = useCallback(() => {
-            setShouldRenderPlaceholder(false);
-        }, [],
-    );
+        setShouldRenderPlaceholder(false);
+    }, []);
+
+
+    let interval: number | undefined;
+    const imgRef = useRef<HTMLImageElement>(null);
+    const onError = useCallback(() => {
+        if (interval) return;
+        
+        let retryNumber = 1;
+        interval = setInterval(() => {
+            if (retryNumber > MAX_RETRIES) {
+                clearInterval(interval);
+                return;
+            }
+
+            retryNumber += 1;
+            console.warn("Image load failed", interval);
+            if (imgRef.current) {
+                imgRef.current.src = src || "";
+                clearInterval(interval);
+                console.log(`image ${src} loaded after ${retryNumber} retries`)
+            }
+        }, 500 * retryNumber * retryNumber);
+    }, []);
+
 
     return (
         <div className={`${styles.container} ${className ? className : ""}`}>
-            <img alt={alt ?? "pic"} onLoad={onLoadHandler} src={src} className={`${styles.image} ${isLoading ? styles.loading : ""}`} {...otherProps}/>
-            { shouldRenderPlaceholder &&
+            <img ref={imgRef}
+                 alt={alt ?? "pic"}
+                 onError={onError}
+                 onLoad={onLoadHandler}
+                 src={src}
+                 className={`${styles.image} ${isLoading ? styles.loading : ""}`} {...otherProps} />
+            {shouldRenderPlaceholder &&
                 <div onTransitionEnd={onTransitionEnd} className={`${styles.placeholder} ${isLoading ? "" : styles.hide}`} />
             }
         </div>
