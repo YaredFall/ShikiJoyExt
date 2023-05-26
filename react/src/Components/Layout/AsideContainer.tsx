bb9221fb078b98c47d520c7cb8de1b315fdf9ce8
YaredFall <yaredfall@gmail.com>
2023-05-27 01:08:26 +0700

    +remove aside panel on smaller screens

--- react/src/Components/Layout/AsideContainer.tsx
+++ react/src/Components/Layout/AsideContainer.tsx
@@ -1,13 +1,31 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import styles from './AsideContainer.module.scss'
+
+
+// 84px + 50rem + 29rem
+const LAYOUT_BREAKPOINT_WIDTH = 84 + 79*16;
 
 type AsideContainerProps = {
     children: ReactNode
 }
 
 const AsideContainer:FC<AsideContainerProps> = ({ children }) => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handler = () => {
+            setWindowWidth(window.innerWidth)
+        };
+        window.addEventListener("resize", handler);
+        
+        return () => {
+            window.removeEventListener("resize", handler);
+        }
+    }, []);
+    
+
     return (
-        <aside className={styles.asideContainer} children={children} />
+        windowWidth >= LAYOUT_BREAKPOINT_WIDTH ? <aside className={styles.asideContainer} children={children} /> : null
     );
 };
 
